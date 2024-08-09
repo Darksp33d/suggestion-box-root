898a0198ae0f4baf70995e9a316dba6d3a8468b9
Saeed <saeed.durrani@vpgcentral.com>
2024-08-09 11:32:43 -0400

    Tweak spacing

--- src/Components/SuggestionLister.jsx
+++ src/Components/SuggestionLister.jsx
@@ -31,7 +31,7 @@ const Suggestion = ({ suggestion }) => (
 //this will show a list of the suggestion cards defined above
 const SuggestionLister = ({ suggestions }) => {
   return (
-    <div className="grid grid-cols-1 sm:grid-cols-2 lg:grid-cols-3 xl:grid-cols-4 gap-4">
+    <div className="grid grid-cols-1 sm:grid-cols-2 lg:grid-cols-3 xl:grid-cols-4 gap-5">
       {suggestions.map((suggestion) => (
         <Suggestion key={suggestion.id} suggestion={suggestion} />
       ))}
